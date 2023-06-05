82d656ec76a4a05c95bf6c826d366591470b8e3b
meDracula <me.dracula@protonmail.com>
2023-06-06 00:40:18 +0200

    Initial command rust main exec git status

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let mut cmd = Command::new("git");
+    cmd.arg("st");
+
+    // Execute the command
+    match cmd.output() {
+        Ok(o) => {
+            unsafe {
+                println!("{}", String::from_utf8_unchecked(o.stdout));
+            }
+        }
+        Err(e) => {
+            println!("That didn't end well! {}", e)
+
+        }
+    }
 }
